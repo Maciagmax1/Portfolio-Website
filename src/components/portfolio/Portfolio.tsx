0cd8a40bced1aa702b53e30d9dd1beba0a9bad87
Max Maciag <maciagmax1@gmail.com>
2023-01-16 15:13:38 -0500

    finished experience and started portfolio

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,28 @@
 import "./Portfolio.css";
 
 const Portfolio = () => {
-  return <section className="Portfolio">Portfolio works</section>;
+  return (
+    <section className="Portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+          <h3>QwikFlix</h3>
+          <a href="https://github.com" className="btn">
+            Github
+          </a>
+          <a
+            href="http://QwikFlix-Demo-Project.surge.sh"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
