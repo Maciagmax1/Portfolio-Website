911307bd2a4918376157b8ce02dc870488301419
Max Maciag <maciagmax1@gmail.com>
2023-01-20 22:30:02 -0500

    finished contact me

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -9,13 +9,13 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>maciagmax1@gmail.com</h5>
             <a href="mailto:maciagmax1@gmail.com">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsLinkedin />
+            <BsLinkedin className="contact__option-icon" />
             <h4>Linkedin</h4>
             <h5>Max Maciag</h5>
             <a href="https://www.linkedin.com/in/max-maciag/" target="_blank">
@@ -23,7 +23,24 @@ const Contact = () => {
             </a>
           </article>
         </div>
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows={7}
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
