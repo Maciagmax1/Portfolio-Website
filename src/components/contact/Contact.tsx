80c11a626e52b5905c29d22f0c357849295dac01
Max Maciag <maciagmax1@gmail.com>
2023-01-20 19:19:30 -0500

    video thumbnail finished

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,32 @@
 import "./Contact.css";
-
+import { MdOutlineEmail } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
 const Contact = () => {
-  return <section className="Contact">Contact works</section>;
+  return (
+    <section className="Contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>maciagmax1@gmail.com</h5>
+            <a href="mailto:maciagmax1@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsLinkedin />
+            <h4>Linkedin</h4>
+            <h5>Max Maciag</h5>
+            <a href="https://www.linkedin.com/in/max-maciag/" target="_blank">
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,28 +1,91 @@
 import "./Portfolio.css";
+import isalloVid from "../../assets/IsalloRunthrough.mp4";
+import qwikFlixVid from "../../assets/QwikFlixRunthrough.mp4";
+import { useState } from "react";
 
 const Portfolio = () => {
+  const [src1, setSrc1] = useState(isalloVid);
+  const [src2, setSrc2] = useState(qwikFlixVid);
+
   return (
     <section className="Portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
         <article className="portfolio__item">
-          <div className="portfolio__item-image"></div>
           <h3>QwikFlix</h3>
-          <a href="https://github.com" className="btn">
-            Github
-          </a>
-          <a
-            href="http://QwikFlix-Demo-Project.surge.sh"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Repellendus fugiat provident necessitatibus. Voluptate, cupiditate
+            repellat eaque ex quod earum. Ab non repudiandae laborum commodi
+            atque ipsa voluptatem numquam ipsum dolores.
+          </p>
+          <div className="btn-container__portfolio">
+            <a
+              href="https://github.com/Maciagmax1/react-api-project-2.git"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="http://QwikFlix-Demo-Project.surge.sh"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+          <div className="project__vid-container">
+            <video
+              className="video"
+              src={src2}
+              width="100% auto"
+              autoPlay
+              muted
+              loop
+            />
+            <video />
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <h3>Isallo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            tenetur voluptates, labore placeat iusto distinctio iure odio cum
+            fuga alias error quibusdam aspernatur blanditiis corporis,
+            perspiciatis facere eum possimus voluptate.
+          </p>
+          <div className="btn-container__portfolio">
+            <a
+              href="https://github.com/zakir17/isallo-frontend.git"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="https://isallo.web.app/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+          <div className="project__vid-container">
+            <video
+              className="video"
+              src={src1}
+              width="100% auto"
+              autoPlay
+              muted
+              loop
+            />
+            <video />
+          </div>
         </article>
       </div>
     </section>
   );
 };
-
 export default Portfolio;
